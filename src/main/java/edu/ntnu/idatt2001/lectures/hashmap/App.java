1a230d43a256dea73e0e12199272a4e668d93e86
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2022-12-13 08:45:41 +0100

    removed target

--- src/main/java/edu/ntnu/idatt2001/lectures/hashmap/App.java
+++ src/main/java/edu/ntnu/idatt2001/lectures/hashmap/App.java
@@ -3,16 +3,19 @@ package edu.ntnu.idatt2001.lectures.hashmap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Logger;
 
 import edu.ntnu.idatt2001.lectures.aggregation.AppData;
 import edu.ntnu.idatt2001.lectures.aggregation.Student;
 
 
-public class StudentHashMapKlient {
+public class App {
+
+	private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
 	public static void main(String[] args) {
 		
-		List<Student> data = new ArrayList<Student>();
+		List<Student> data = new ArrayList<>();
 		data = AppData.getData();
 
 		HashMap<String,Student> studentHashmap = new HashMap<String,Student>();
@@ -24,10 +27,10 @@ public class StudentHashMapKlient {
 
 
 		String studentId = "1998-03-15-DelFinito-Huppasahn-F";
-		System.out.println(studentHashmap.containsKey(studentId)); 
+		LOGGER.info(studentHashmap.containsKey(studentId));
 
 		studentHashmap.remove(studentId); 
-		System.out.println(studentHashmap.containsKey(studentId)); 		
+		LOGGER.info(studentHashmap.containsKey(studentId));
 	}
 }
 

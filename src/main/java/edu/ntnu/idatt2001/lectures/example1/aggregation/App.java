225719c4a194b275bb1c049c4e003bee12631136
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2023-05-10 09:00:11 +0200

    javadoc

--- src/main/java/edu/ntnu/idatt2001/lectures/example1/aggregation/App.java
+++ src/main/java/edu/ntnu/idatt2001/lectures/example1/aggregation/App.java
@@ -5,11 +5,20 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
+/**
+ * App.java - "Programmering i Java", 4.utgave - 2009-07-01
+ * <p>
+ * Klasse som inneholder main-metoden for applikasjonen.
+ */
 public class App {
-
+	// Logger for å skrive ut informasjon til konsollen
 	private static final Logger logger = Logger.getLogger(App.class.getName());
 
+	/**
+	 * Main-metode som starter applikasjonen.
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		logger.log(Level.INFO,"Logger Name: {0}",logger.getName());
